20e3cf9f9d79a1008ea80c2aafa551d16575dba0
Yash <ypt7895@gmail.com>
2020-08-18 10:54:33 +0530

    Added boilerplate code

---
+++ script.js
@@ -0,0 +1,7 @@
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 480;
+canvas.height = 360;
+canvas.style.backgroundColor = "black";
+document.body.appendChild(canvas);
+
