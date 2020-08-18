4d160392a44f4a9b62a5fedc588908ffcbd8ba41
Yash <ypt7895@gmail.com>
2020-08-18 11:03:52 +0530

    added objects

--- script.js
+++ script.js
@@ -5,3 +5,39 @@ canvas.height = 360;
 canvas.style.backgroundColor = "black";
 document.body.appendChild(canvas);
 
+//constants for the game
+const paddleHeight = 80;
+const paddleWidth = 15;
+const dy = 2.85;
+
+//score for players
+var score = {
+    p1 = 0,
+    p2 = 0
+};
+
+//storing events
+var events = {};
+addEventListener("keydown",function(e){
+    events[e.keyCode]=true;
+},false);
+addEventListener("keyup",function(e){
+    delete events[e.keyCode];
+},false);
+
+var ball = {
+    x = 240,
+    y = 1 + (Math.random()*358),
+    Xmag = 1.5,
+    Ymag = 1
+};
+
+var player1 = {
+    x = 30,
+    y = 240
+};
+
+var player1 = {
+    x = 435,
+    y = 240
+};
